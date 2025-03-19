02a55ee86643e98e3372c33aee6b6035195dcbf5
VineshRajkumar <vineshrajkumar23@gmail.com>
2025-03-19 16:58:22 +0530

    Added base in vite.config.json and Change name of repository to Brown

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: "/Brown/",
   build:{
     rollupOptions:{
       output:{
