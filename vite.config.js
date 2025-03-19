f91e0926093ecdcb5eec793bcab1d6bdc4a716f2
VineshRajkumar <vineshrajkumar23@gmail.com>
2025-03-19 15:37:19 +0530

    Changes made in vite.config.js and package.json

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,25 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build:{
+    rollupOptions:{
+      output:{
+        manualChunks(id){
+          if(id.includes("node_modules")){
+            if (id.includes("react-dom/server")) return "react-dom-server";
+            if (id.includes("react-dom/client")) return "react-dom-client";
+            if (id.includes("react-dom")) return "react-dom";
+            return "vendor";
+          }
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
