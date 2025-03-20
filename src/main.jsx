d5d131d774992e7bc4b64c9a9635abbff8d22a59
VineshRajkumar <vineshrajkumar23@gmail.com>
2025-03-20 21:29:37 +0530

    Some changes made in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 //createBrowserRouter not supported for gh-pages 
 //so using createhashrouter
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -19,7 +19,7 @@ import Home from './app/home/page'
 import { Protected } from './components'
 import NotFoundPage from './app/not-found/page'
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout/>,
